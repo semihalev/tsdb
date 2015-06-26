08f43ebea2736fdda7febe5285f004d915494d96
semihalev <semihalev@gmail.com>
2015-06-26 17:09:24 +0300

    adding php client

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Small Time Series Database
+# Small Time Series Database
+
+TSDB is small NoSQL database with [BoltDB](https://github.com/boltdb/bolt) as backend
+
+## Features
++ HTTP API support
+
+## Build and Install
+go get github.com/semihalev/tsdb
+
+cd go/src/github.com/semihalev/tsdb
+
+go build
+
+## API Example
+
+Query Series:
+curl http://127.0.0.1:4080/api/v1/query?series=world
+
+Write Series:
+curl http://127.0.0.1:4080/api/v1/write?series=world&value=hello
+
+Count Series
+curl http://127.0.0.1:4080/api/v1/count?series=world
+
+Delete Series:
+curl http://127.0.0.1:4080/api/v1/delete?series=world
+
+Delete by Time:
+curl http://127.0.0.1:4080/api/v1/deletebytime?series=world&time=1435184955779847472
+
+Backup DB:
+curl http://127.0.0.1:4080/api/v1/backup
+
+Stats DB:
+curl http://127.0.0.1:4080/api/v1/stats
+
+## PHP Client
+
+tsdb::query()
+tsdb::write()
+tsdb::count()
+tsdb::delete()
+tsdb::deletebytime()
+
---
+++ phpclient/tsdb.php
@@ -0,0 +1,84 @@
+<?php
+
+abstract class tsdb {
+	protected static $host = "localhost";
+	protected static $port = "4080";
+	protected static $base = "api/v1/";
+
+	public static function query( $series, $limit = 0, $offset = 0, $order = "desc" ) {
+		$ch = self::getCurl( "query", array( "series" => $series, "limit" => $limit, "offset" => $offset, "order" => $order ) );
+		$result = self::execCurl( $ch, true );
+		if ( isset( $result["status"] ) && $result["status"] == "error" ) {
+			throw new \RuntimeException( $result["message"] );
+		}
+
+		return $result["result"];
+	}
+
+	public static function write( $series, $value ) {
+		$ch = self::getCurl( "write", array( "series" => $series, "value" => $value ) );
+		$result = self::execCurl( $ch, true );
+
+		if ( isset( $result["status"] ) && $result["status"] == "error" ) {
+			throw new \RuntimeException( $result["message"] );
+		}
+
+		return true;
+	}
+
+	public static function delete( $series ) {
+		$ch = self::getCurl( "delete", array( "series" => $series ) );
+		$result = self::execCurl( $ch, true );
+
+		if ( isset( $result["status"] ) && $result["status"] == "error" ) {
+			throw new \RuntimeException( $result["message"] );
+		}
+
+		return true;
+	}
+
+	public static function deletebytime( $series, $time ) {
+		$ch = self::getCurl( "deletebytime", array( "series" => $series, "time" => $time ) );
+		$result = self::execCurl( $ch, true );
+
+		if ( isset( $result["status"] ) && $result["status"] == "error" ) {
+			throw new \RuntimeException( $result["message"] );
+		}
+
+		return true;
+	}
+
+	public static function count( $series ) {
+		$ch = self::getCurl( "count", array( "series" => $series ) );
+		$result = self::execCurl( $ch, true );
+
+		if ( isset( $result["status"] ) && $result["status"] == "error" ) {
+			throw new \RuntimeException( $result["message"] );
+		}
+
+		return $result["result"];
+	}
+
+	protected static function getCurl( $uri, array $args = array() ) {
+		$url  = "http://".self::$host.":".self::$port."/".self::$base."".$uri;
+		$url .= "?" . http_build_query( $args );
+		$ch   = curl_init( $url );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 4 );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
+		return $ch;
+	}
+
+	protected static function execCurl( $ch, $json = false ) {
+		$response = curl_exec( $ch );
+		$status   = (string)curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+		//$type     = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+		curl_close( $ch );
+		if ( $status[0] != 2 ) {
+			throw new \RuntimeException( $response );
+		}
+		return $json ? json_decode( $response, true ) : $response;
+	}
+}
+
+?>
